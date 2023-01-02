bc18153d1ad02237306dcd2a439b46fd5112f4b1
Wisnu Setiadi <wisnuarisetiadi@gmail.com>
2023-01-02 13:34:54 +0000

    remove export token

--- karsajobs-be/build_push_image_karsajobs.sh
+++ karsajobs-be/build_push_image_karsajobs.sh
@@ -9,8 +9,6 @@ docker tag karsajobs:latest ghcr.io/ariecc101/karsajobs:latest
 #add new line to tidy output
 echo '\n\n'
 
-#export github token to env
-export GITHUB_PACKAGES_TOKEN=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
 #login to github packages
 echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u ariecc101 --password-stdin
 #add new line to tidy output
--- karsajobs-fe/build_push_image_karsajobs_ui.sh
+++ karsajobs-fe/build_push_image_karsajobs_ui.sh
@@ -9,8 +9,6 @@ docker tag karsajobs-ui:latest ghcr.io/ariecc101/karsajobs-ui:latest
 #add new line to tidy output
 echo '\n\n'
 
-#export github token to env
-export GITHUB_PACKAGES_TOKEN=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
 #login to github packages
 echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u ariecc101 --password-stdin
 #add new line to tidy output
